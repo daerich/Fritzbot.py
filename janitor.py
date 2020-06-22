3e9efcf4032bd66dbbdc255923d881c112a04d83
DaErich <fakefakefans@gmail.com>
2020-06-22 20:21:32 +0200

    added ini file, softcoded exclusion role

--- janitor.py
+++ janitor.py
@@ -1,16 +1,19 @@
+import configparser
 from filereader import filereader
 
 class Janitor:
 
     ##__bannedWords = ["sex"] 
     __bannedWords = filereader("list.txt").read()
+    __config = configparser.ConfigParser()
+    __config.read("bot.ini")
 
     async def delete(self, message):
         await message.delete()
     
     async def sweep(self, message): 
         for val in self.__bannedWords:
-            if val in message.content and self.checkForRole(message, "YOURROLEHERE") is False:
+            if val in message.content and self.checkForRole(message, self.__config.get('ROLES', 'Moderator')) is False:
                 await self.delete(message)
                 print("Message deleted!")
     
